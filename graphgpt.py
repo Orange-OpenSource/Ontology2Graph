064a05953d92befe8ba7c988bab46018e6ba7e11
philippe.dooze <philippe.dooze@orange.com>
2025-05-21 10:34:44 +0200

    first  commit

---
+++ graphgpt.py
@@ -0,0 +1,40 @@
+""" Generate Triplets based on turtle file, using the internal Orange LLM Proxy 
+portal (https://portal.llmproxy.ai.orange/)"""
+
+import os
+from openai import OpenAI, OpenAIError
+
+client = OpenAI(api_key=os.environ.get("ORANGE_LLM_PROXY_KEY"),
+                base_url="https://llmproxy.ai.orange")
+
+with open('schema.ttl','rt',encoding='utf-8') as TTL:
+    TTL_SCHEMA = ','.join(str(x) for x in TTL.readlines())
+
+with open('relation_info.json','rt',encoding='utf-8') as rel:
+    RELATIONS = ','.join(str(x) for x in rel.readlines())
+
+with open('instructions2.txt','rt',encoding='utf-8') as file_instructions:
+    INSTRUCTION = ','.join(str(x) for x in file_instructions.readlines())
+
+try:
+    response = client.chat.completions.create(
+        #model="openai/gpt-3.5-turbo",
+        model="vertex_ai/gemini-2.0-flash",
+        temperature=0.1, # reducing model's creativity
+        top_p=0.1, # reducing model's creativity
+        messages = [
+            {   "role":"system",
+                "content":"""You are an expert in websemantic technologies and most particulary in knowledge graph"""
+            },
+            {   "role": "user",
+                "content": f"""Follow the instruction : {INSTRUCTION} and use the following schema:
+                {TTL_SCHEMA} and relations description {RELATIONS} to provide the description of the knowledge graph in turtle format"""
+            }
+        ]
+    )
+    with open('response.txt','w',encoding='utf-8') as f:
+        f.write(response.choices[0].message.content)
+    print(response.choices[0].message.content)
+
+except OpenAIError as e:
+    print(f"An error occured: {e}")
