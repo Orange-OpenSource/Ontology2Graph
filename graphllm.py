294cf140d8106ce1e27748ea1e21b43d61f92f32
philippe dooze <philippe.dooze@orange.com>
2025-06-10 15:15:22 +0200

    minor change

--- graphllm.py
+++ graphllm.py
@@ -4,8 +4,25 @@ portal (https://portal.llmproxy.ai.orange/)"""
 import os
 from openai import OpenAI, OpenAIError
 
-GENERATED_GRAPH = 'results/Third_generated_graph2.ttl'
-
+PATH="/mnt/c/Users/piod7321/Downloads/results"
+MODEL="vertex_ai/gemini-2.0-flash" 
+#MODEL='openai/gpt-4.1-mini',
+#Model='vertex_ai/claude3.7-sonnet',
+#Model='openai/o1-preview',
+#Model='openai/o3',
+#Model='openai/gpt-4.1-nano',
+#Model='openai/gpt-4.1',
+#Model='vertex_ai/gemini-1.5-flash',
+#Model='openai/o4-mini',
+#Model='openai/gpt-4o',
+#Model='vertex_ai/gemini-1.5',
+#Model="openai/gpt-4o-mini",
+#Model="openai/o3-mini",
+#Model="vertex_ai/claude3.5-sonnet-v2",
+#Model="openai/o1",
+#Model="vertex_ai/codestral",
+#Model="openai/o1-mini",
+#Model="openai/gpt-3.5-turbo",
 
 client = OpenAI(api_key=os.environ.get("ORANGE_LLM_PROXY_KEY"),
                 base_url="https://llmproxy.ai.orange")
@@ -21,10 +38,7 @@ with open('full_graph.ttl','rt',encoding='utf-8') as file_instructions:
 
 try:
     response = client.chat.completions.create(
-        #model="openai/gpt-3.5-turbo",
-        #model="vertex_ai/gemini-2.0-flash",
-        #model="openai/o1-mini",
-        model="openai/gpt-4.1-nano",
+        model=MODEL,
         temperature=0.1, # increase model's creativity
         top_p=0.1, # increase model's creativity
         messages = [
@@ -38,12 +52,11 @@ try:
         ]
     )
     
-    with open(GENERATED_GRAPH,'w',encoding='utf-8') as f:
+    with open(PATH + f'/Third_graph_{response.model}.ttl','w',encoding='utf-8') as f:
         f.write(response.choices[0].message.content)
 
-
 ## remove useless character
-    with open(GENERATED_GRAPH, 'r') as file:
+    with open(f'/mnt/c/Users/piod7321/Downloads/results/Third_graph_{response.model}.ttl', 'r') as file:
         lines = file.readlines()
 
     # Check if the first line starts with the specific character
@@ -54,12 +67,12 @@ try:
         lines = lines[:-1]
 
         # Write the remaining lines back to the file
-        with open(GENERATED_GRAPH, 'w') as file:
+        with open(f'/mnt/c/Users/piod7321/Downloads/results/Third_graph_{response.model}.ttl', 'w') as file:
             file.writelines(lines)
 
 ## print file content
 
-    with open(GENERATED_GRAPH, 'r') as file:
+    with open(f'/mnt/c/Users/piod7321/Downloads/results/Third_graph_{response.model}.ttl', 'r') as file:
         contents = file.read()
         print(contents)
 
