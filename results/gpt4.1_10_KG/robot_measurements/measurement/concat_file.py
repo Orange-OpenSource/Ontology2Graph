ea4339446ee13f637a41b8c001e47731082a5d23
philippe dooze <philippe.dooze@orange.com>
2025-06-24 11:38:44 +0200

    columns removes

--- results/gpt4.1_10_KG/robot_measurements/measurement/concat_file.py
+++ results/gpt4.1_10_KG/robot_measurements/measurement/concat_file.py
@@ -24,6 +24,20 @@ for file in matching_files:
     # Append the data to the concatenated DataFrame
     concatenated_df = pd.concat([concatenated_df, df], axis=0,ignore_index=True)
 
+# Remove some columns
+df_final=concatenated_df.drop(columns=['abox_nominals','abox_nominals_incl','axiom_type_count','axiom_type_count.1',
+                                       'axiom_type_count_incl','axiom_type_count_incl.1','axiom_types','axiom_types.1'
+                                       ,'axiom_types_incl','axiom_types_incl.1','axiom_types_incl','certain_cycle',
+                                       'certain_cycle_incl','class_expression_count','class_expression_count_incl',
+                                       'curie_map','curie_map.1','curie_map.2','datatypes_builtin','datatypes_builtin_incl',
+                                       'namespace_axiom_count','namespace_axiom_count.1','namespace_axiom_count_incl',
+                                       'namespace_axiom_count_incl.1','namespace_entity_count','namespace_entity_count.1',
+                                       'namespace_entity_count.2','namespace_entity_count.3','namespace_entity_count_incl',
+                                       'namespace_entity_count_incl.1','namespace_entity_count_incl.2',
+                                       'namespace_entity_count_incl.3','ontology_iri','ontology_version_iri','owl2',
+                                       'owl2_dl','owl2_el','owl2_ql','owl2_rl','owl2dl_profile_violation',
+                                       'owl2dl_profile_violation.1','rdfs','syntax','tbox_nominals','tbox_nominals_incl'])
+
 # Write the concatenated DataFrame to a new CSV file
-concatenated_df.to_csv(FILE_NAME, index=False)
+df_final.to_csv(FILE_NAME, index=False)
 print(f"CSV files have been concatenated into {FILE_NAME} without additional headers.")
