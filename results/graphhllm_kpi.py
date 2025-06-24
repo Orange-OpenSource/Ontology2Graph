de4074c44fbe505aca86ccbcec686e525b691209
philippe dooze <philippe.dooze@orange.com>
2025-06-24 10:50:33 +0200

    metric

--- results/graphhllm_kpi.py
+++ results/graphhllm_kpi.py
@@ -26,7 +26,7 @@ date_string2 = replace_char_at_index(date_string1, 16, ':')
 # Convert the string to a datetime object
 date = datetime.strptime(date_string2, DATE_FORMAT)
 
-'transposing initila data and concat the result'
+'transposing initial data and concat the result'
 
 # Load the CSV file into a DataFrame
 input_file = PATH+initial_date+'_gpt-4.1-robot_measurement.csv'
@@ -35,9 +35,10 @@ output_file = PATH+initial_date+'_gpt-4.1-robot_measurement_transposed.csv'
 # Read the CSV file
 df = pd.read_csv(input_file)
 
-# replace metric_value by the date
+# remove & rename columns and KPIs
 del df['metric_type']
 df = df.rename(columns={'metric_value':date})
+df = df.rename(columns={'metric':'date'})
 
 # Transpose the DataFrame
 df_transposed = df.transpose()
@@ -48,18 +49,3 @@ df_transposed.to_csv(output_file, header=False)
 
 print(f"File transposed and saved as {output_file}")
 
-
-# Load the data
-#df = pd.read_csv('/home/pdooze/DIGITAL_TWIN/GenGraphLLM/results/correct/Third_graph_gemini-1.5-flash.ttl')
-
-# Supposons que ton fichier CSV a des colonnes nommées 'Date' et 'Valeur'
-# Remplace 'Date' et 'Valeur' par les noms de colonnes appropriés de ton fichier CSV
-# plt.plot(df['Date'], df['Valeur'])
-
-# Ajouter des titres et des labels
-# plt.title('Exemple de Line Chart')
-# plt.xlabel('Date')
-# plt.ylabel('Valeur')
-
-# Afficher le graphique
-#plt.show()
