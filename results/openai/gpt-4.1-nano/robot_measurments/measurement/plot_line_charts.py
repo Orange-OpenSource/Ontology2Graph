316fe59b91328b462edcac4ac16f3bc8c01d228c
philippe dooze <philippe.dooze@orange.com>
2025-06-25 11:00:18 +0200

    fix error on plots

--- results/openai/gpt-4.1-nano/robot_measurments/measurement/plot_line_charts.py
+++ results/openai/gpt-4.1-nano/robot_measurments/measurement/plot_line_charts.py
@@ -10,9 +10,21 @@ import sys
 FILE_NAME = 'concatenated_file.csv'
 Y_NAME = sys.argv[1:]
 
-df = pd.read_csv(FILE_NAME)
+df = pd.read_csv((FILE_NAME))#,index_col='date',parse_dates=True)
+df['date_time'] = pd.to_datetime(df['date'],format='%Y-%m-%d %H:%M:%S',errors="coerce")
+df=df.sort_values(by=['date_time'])
+df=df.drop('date',axis="columns")
+df= df.set_index('date_time')
 
-df.plot(x='date',y=Y_NAME)
-plt.savefig(Y_NAME[0], dpi=300)
+print(df)
+
+#ligne = df.iloc[1]
+# Afficher la valeur dans la colonne 'Nom' pour cette ligne
+#print(type(ligne['date']))
+#print(type(ligne['date_time']))
+#print(df.dtypes)
+
+df.plot(y=Y_NAME)
+#plt.savefig(Y_NAME[0], dpi=300)
 plt.show()
 
