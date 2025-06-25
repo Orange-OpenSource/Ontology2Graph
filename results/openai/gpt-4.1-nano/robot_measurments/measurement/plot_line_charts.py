af1b32dc9c0c1b0f032b005287d97a589306dcce
philippe dooze <philippe.dooze@orange.com>
2025-06-25 15:17:09 +0200

    line charts

--- results/openai/gpt-4.1-nano/robot_measurments/measurement/plot_line_charts.py
+++ results/openai/gpt-4.1-nano/robot_measurments/measurement/plot_line_charts.py
@@ -10,21 +10,15 @@ import sys
 FILE_NAME = 'concatenated_file.csv'
 Y_NAME = sys.argv[1:]
 
-df = pd.read_csv((FILE_NAME))#,index_col='date',parse_dates=True)
-df['date_time'] = pd.to_datetime(df['date'],format='%Y-%m-%d %H:%M:%S',errors="coerce")
-df=df.sort_values(by=['date_time'])
-df=df.drop('date',axis="columns")
-df= df.set_index('date_time')
+df = pd.read_csv((FILE_NAME))
 
-print(df)
+df['date'] = pd.to_datetime(df['date'],format='%Y-%m-%d %H:%M:%S',errors="coerce")
+df=df.sort_values(by=['date'])
+df= df.set_index('date')
 
-#ligne = df.iloc[1]
-# Afficher la valeur dans la colonne 'Nom' pour cette ligne
-#print(type(ligne['date']))
-#print(type(ligne['date_time']))
-#print(df.dtypes)
-
-df.plot(y=Y_NAME)
-#plt.savefig(Y_NAME[0], dpi=300)
+plt.plot(df.index,df[Y_NAME])
+plt.xlabel('Date')
+plt.title(Y_NAME)
+plt.savefig(Y_NAME[0], dpi=300)
 plt.show()
 
