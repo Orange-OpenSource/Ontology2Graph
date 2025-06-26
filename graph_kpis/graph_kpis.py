83d82a4e1d10f6d1d32632f9f841f14c1c30bde3
philippe dooze <philippe.dooze@orange.com>
2025-06-26 14:28:45 +0200

    networkx

--- graph_kpis/graph_kpis.py
+++ graph_kpis/graph_kpis.py
@@ -1,30 +1,29 @@
 ''' This python script compute some Knowledge Graph KPIs '''
-from rdflib.extras.external_graph_libs import *
-from rdflib import Graph, URIRef, Literal
+from rdflib import Graph
 import networkx as nx
-import numpy as np
-import matplotlib.pyplot as plt
+import csv
 
 
 #constant
 PATH = "../comparison_model_results/Noria/"
+FILE = "First_graph_noria_2025-06-20_16-20-20_gemini-2.0-flash-001.ttl"
 
 #load the graph
 graph = Graph()
-graph.parse(f'{PATH}/First_graph_noria_2025-06-20_16-20-20_gemini-2.0-flash-001.ttl', format="turtle")
+graph.parse(f'{PATH}/{FILE}', format="turtle")
 #nx_graph = rdflib_to_networkx_multidigraph(graph)
-nx_graph = nx.DiGraph(graph)
+#nx_graph = nx.DiGraph(graph)
+nx_graph = nx.MultiDiGraph(graph)
 
 nbr_nodes=nx_graph.number_of_nodes()
 nbr_edges=nx_graph.number_of_edges()
 nodes=list(nx_graph.nodes())
 
+with open('output.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    for item in nodes:
+            writer.writerow([item])
 
 print(f"Number of nodes: {nbr_nodes}")
 print(f"Number of edges: {nbr_edges}")
-print(f"Nodes in the graph: {nodes}")
-
-
-
-
-
+#print(f"Nodes in the graph: {nodes}")
