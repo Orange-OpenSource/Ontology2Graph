afcf831bf7790c01827d0b33dcff4acbeb9bdc50
philippe dooze <philippe.dooze@orange.com>
2025-07-01 10:56:31 +0200

    new charts

--- graphs_time_series_generated/plot_line_charts.py
+++ graphs_time_series_generated/plot_line_charts.py
@@ -1,22 +1,36 @@
 
 ''' This script plots line chart from concatenated file. The name of the KPI
 must be use as a first argument and the location of the concatenated file 
-as a second argument when laucnhing the python script'''
+(compelte folder PATH + the file itself as a second argument when laucnhing 
+the python script'''
 
-import plotly.graph_objects as go
-import plotly.express as px
-import pandas as pd
 import sys
+import plotly.express as px
+#import kaleido
+#from kaleido import write_fig_sync
+import pandas as pd
+#import os
+
+#kaleido.get_chrome_sync
 
 # use argument as constant
 Y_NAME = sys.argv[1]
-FILE_NAME = sys.argv[2]
+PATH_FILE = sys.argv[2]
 
-df = pd.read_csv((FILE_NAME))
+#Construct the SAVE_PATH to store the result
+parts=PATH_FILE.split("/")
+PATH=""
+
+for i in range(len(parts)-2) :
+    PATH = f'{PATH}/{parts[i+1]}'
+
+SAVE_PATH=f'{PATH}/Charts'
+
+#Load the data and process them
+df = pd.read_csv((PATH_FILE))
 
 df['date'] = pd.to_datetime(df['date'],format='%Y-%m-%d %H:%M:%S',errors="coerce")
 df=df.sort_values(by=['date'])
-#df= df.set_index('date')
 
 fig = px.line(df, x='date', y=Y_NAME, markers=True)
 
@@ -26,29 +40,28 @@ fig.update_layout(
         'font': {
             'size': 24,
             'color': 'blue',
-            'family': 'Arial',
-            'weight': 'bold'
+            'family': 'Arial'
         },
-        'x': 0.5,  # Center the title
+        'x': 0.5,  # Center the titleS
         'xanchor': 'center',
         'yanchor': 'top'
     }
 )
 
-
 fig.show()
 
+#Save file
+#if not os.path.exists(SAVE_PATH):
+#    os.makedirs(f'{SAVE_PATH}')
 
+#print(f'{SAVE_PATH}/{Y_NAME}.svg')
 
+#kaleido.write_fig_sync(fig, path=f"{SAVE_PATH}/{Y_NAME}.png")
 
-#fig = go.Figure()
-#fig.add_trace(go.Scatter(
-#     x=df.index,
-#     y=df[Y_NAME],
-#     mode='lines+markers'
-#     ))
+#fig.write_image(f"{SAVE_PATH}/{Y_NAME}.svg")
 
-#fig.update_layout(title=
-#                  {'text' : f'time serie {Y_NAME}',
-#                   'font' : {'size': 24, 'family': 'Arial', 'weight': 'bold'}})
-#fig.show()
+#Dysplay in firefox
+#HTML_FILE=f'{SAVE_PATH}/{Y_NAME}.html'
+
+#firefox_path = webbrowser.get("firefox")
+#firefox_path.open('file://' + os.path.realpath(HTML_FILE))
