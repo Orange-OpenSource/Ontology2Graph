2a30b705856e29b8110657f79728ac410f128719
philippe.dooze <philippe.dooze@orange.com>
2025-07-02 11:05:07 +0200

    new enhancements

--- graphllm.py
+++ graphllm.py
@@ -1,6 +1,8 @@
-""" Generate knowledge graph in turtle format based on schema, using the internal Orange LLM Proxy 
-portal (https://portal.llmproxy.ai.orange/)"""
-
+""" This python script generates knowledge graph in turtle format based on an ontology schema, a 
+prompt and/or a graph example. It uses the internal Orange LLM Proxy portal 
+(https://portal.llmproxy.ai.orange/) for which you must have an account. Several constant must be
+set directly in this script before launching
+"""
 import os
 import datetime
 import subprocess
@@ -15,21 +17,16 @@ PATH_ONTOLOGY=f'{PATH}/ontologies'
 PATH_PROMPT=f'{PATH}/prompt'
 PATH_GRAPH=f'{PATH}/graph'
 
-
-
-#PROMPT_TYPE to choose
+## PROMPT_TYPE to choose ##
 PROMPT_TYPE='First_prompt'
 #PROMPT_TYPE='Second_prompt'
 #PROMPT_TYPE='Third_prompt'
 
-#PROMPT type
-PROMPT_FILE=f'{PROMPT_TYPE}.txt'
-
-#ONTOLOGY type to choose
+## ONTOLOGY to choose ##
 ONTO='Noria'
 #ONTOLOGY='pygraph'
 
-#MODEL constant to choose
+## MODEL constant to choose ##
 
 MODEL="vertex_ai/gemini-2.0-flash"
 #MODEL='openai/gpt-4.1-mini'
@@ -51,20 +48,19 @@ MODEL="vertex_ai/gemini-2.0-flash"
 #MODEL="openai/gpt-3.5-turbo"
 
 #PATH result to choose
-PATH_RESULT=f'{PATH}/graphs_generated_by_models/{MODEL}'
-#PATH_RESULT=f'{PATH}/graphs_series_generated'
+#PATH_RESULT=f'{PATH}/graphs_generated_by_models/{MODEL}'
+PATH_RESULT=f'{PATH}/graphs_time_series_generated/{MODEL}'
 
-os.makedirs(f'{PATH_RESULT}/{MODEL}/',exist_ok=True)
+os.makedirs(f'{PATH_RESULT}/',exist_ok=True)
 
 BAD_PATH_RESULT=f'{PATH_RESULT}/Bad_Turtle_Syntax'
 
-client = OpenAI(api_key=os.environ.get("ORANGE_LLM_PROXY_KEY"),
-                base_url="https://llmproxy.ai.orange")
+client = OpenAI(api_key=os.environ.get("LLM_PROXY_KEY"),base_url="https://llmproxy.ai.orange")
 
 with open(f'{PATH_ONTOLOGY}/Noria.ttl','rt',encoding='utf-8') as ontology:
     ONTOLOGY = ','.join(str(x) for x in ontology.readlines())
 
-with open(f'{PATH_PROMPT}/{PROMPT_FILE}','rt',encoding='utf-8') as prompt:
+with open(f'{PATH_PROMPT}/{PROMPT_TYPE}.txt','rt',encoding='utf-8') as prompt:
     PROMPT = ','.join(str(x) for x in prompt.readlines())
 
 # Only for second and third prompt
@@ -74,8 +70,8 @@ with open(f'{PATH_GRAPH}/full_graph.ttl','rt',encoding='utf-8') as graph:
 try:
     response = client.chat.completions.create(
         model=MODEL,
-        temperature=0.1, # increase model's creativity
-        top_p=0.1, # increase model's creativity
+        temperature=0.1, # model's creativity
+        top_p=0.1, # model's creativity
         messages = [
             {   "role":"system",
                 "content":"""You are an expert in websemantic technologies and most 
@@ -89,35 +85,28 @@ try:
     )
 
     #Write the result in a temporary file
-    with open(f'{PATH_RESULT}/{PROMPT_TYPE}_temp.ttl'
-              ,'w',encoding='utf-8') as filetemp:
+    with open(f'{PATH_RESULT}/{PROMPT_TYPE}_temp.ttl','x',encoding='utf-8') as filetemp:
         filetemp.write(response.choices[0].message.content)
         filetemp.close()
 
     #Remove lines start with '
-    with open(f'{PATH_RESULT}/{PROMPT_TYPE}_temp.ttl'
-              ,'r',encoding='utf-8') as file:
+    with open(f'{PATH_RESULT}/{PROMPT_TYPE}_temp.ttl','r',encoding='utf-8') as file:
         lines = file.readlines()
         filtered_lines = [lines for lines in lines if not lines.startswith('`')]
-        #save filtered result
-        with open(f'{PATH_RESULT}/{PROMPT_TYPE}_{DATE}_{ONTO}.ttl'
-                    ,'w',encoding='utf-8') as filtered_file:
-            filtered_file.writelines(filtered_lines)
-            filtered_file.close()
-
         file.close()
-        os.remove(f'{PATH_RESULT}/{PROMPT_TYPE}_temp.ttl')
+
+    #save filtered result
+    with open(f'{PATH_RESULT}/{PROMPT_TYPE}_{DATE}_{ONTO}.ttl','w',encoding='utf-8') as filtered:
+        filtered.writelines(filtered_lines)
+        filtered.close()
+
+    os.remove(f'{PATH_RESULT}/{PROMPT_TYPE}_temp.ttl')
 
     # Check Turtle syntax
-    #command=["ttl",f'{PATH_RESULT}/{PROMPT_TYPE}_{DATE}_{ONTO}.ttl']
-    command=["ttl",f'{PATH_RESULT}/First_prompt_2025-07-01_15-33-33_Noria_BAD.ttl']
-    turtlevalidator=subprocess.Popen(command, stdout=subprocess.PIPE,
-                                         stderr=subprocess.PIPE,text=True)
-    stdout, stderr = turtlevalidator.communicate()
-    print("Turtle validator Result:")
-    print(stdout)
-    print(stderr)
-    print(turtlevalidator.communicate())
+    command=["ttl",f'{PATH_RESULT}/{PROMPT_TYPE}_{DATE}_{ONTO}.ttl']
+    ttlvalidator=subprocess.Popen(command, stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+    stdout, stderr = ttlvalidator.communicate()
+    print(f'Turtle validator Result: {ttlvalidator.communicate()}')
 
     if stdout!='Validator finished with 0 warnings and 0 errors.\n' :
         # move bad file in bad folder and Save logs
@@ -126,9 +115,9 @@ try:
         shutil.move(f'{PATH_RESULT}/{PROMPT_TYPE}_{DATE}_{ONTO}.ttl',
                     f'{BAD_PATH_RESULT}/{PROMPT_TYPE}_{DATE}_{ONTO}_BAD.ttl')
 
-        with open(f'{PATH_RESULT}/Bad_Turtle_Syntax/errors.log', 'r+',
+        with open(f'{BAD_PATH_RESULT}/errors.log', 'a',
                        encoding='utf-8') as log:
-            log.write(f'{PROMPT_TYPE}_{DATE}_{ONTO}_BAD.ttl : {turtlevalidator.communicate()}\n')
+            log.write(f'{PROMPT_TYPE}_{DATE}_{ONTO}_BAD.ttl : {ttlvalidator.communicate()}\n')
             log.close()
 
 except OpenAIError as e:
