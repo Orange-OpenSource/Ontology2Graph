3890cb9ccf1db73a5f323247b8aa526e1aa743d8
philippe dooze <philippe.dooze@orange.com>
2025-07-07 18:01:29 +0200

    remove tbox predicate

--- graphs_kpis/NetworkX/graph_kpis.py
+++ graphs_kpis/NetworkX/graph_kpis.py
@@ -4,13 +4,13 @@ location folder where are stored all the ttl files'''
 import sys
 import os
 from pathlib import Path
+import webbrowser
 import networkx as nx
 import rdflib
-from networkx.algorithms import *
+#from networkx.algorithms import *
 from networkx.classes.function import degree_histogram
-#approximation, clique, connectivity, average_node_connectivity
+from networkx import approximation
 from pyvis.network import Network
-import webbrowser
 
 arg = sys.argv[1:]
 PATH= arg[0]
@@ -36,29 +36,79 @@ for file in all_files :
     DiGraph = nx.DiGraph()
     Graph = nx.Graph()
 
+    #my_subj=[]
+    #my_pred=[]
+    #my_obj=[]
+
     for subj, pred, obj in g:
 
-        #print('subj',subj)
-        #print('pred',pred)
-        #print('object',obj)
+        # Add nodes
+        if 'comment' in pred :
+            edges_to_remove = [(u, v) for u, v, attr in Graph.edges(data=True)
+            if attr.get('label') == pred and v == obj]
+            Graph.remove_edges_from(edges_to_remove)
 
-        # Add nodes (optional, as adding edges will add nodes automatically)
-        DiGraph.add_node(str(subj))
-        DiGraph.add_node(str(obj))
-        DiGraph.add_edge(str(subj),str(obj),label=str(pred))
+            edges_to_remove = [(u, v) for u, v, attr in DiGraph.edges(data=True)
+            if attr.get('label') == pred and v == obj]
+            DiGraph.remove_edges_from(edges_to_remove)
 
-        Graph.add_node(str(subj))
-        Graph.add_node(str(obj))
-        Graph.add_edge(str(subj),str(obj),label=str(pred))
-   
+        elif 'label' in pred :
+            edges_to_remove = [(u, v) for u, v, attr in Graph.edges(data=True)
+            if attr.get('comment') == pred and v == obj]
+            Graph.remove_edges_from(edges_to_remove)
+
+            edges_to_remove = [(u, v) for u, v, attr in DiGraph.edges(data=True)
+            if attr.get('comment') == pred and v == obj]
+            DiGraph.remove_edges_from(edges_to_remove)
+
+        elif 'type' in pred :
+            edges_to_remove = [(u, v) for u, v, attr in Graph.edges(data=True)
+            if attr.get('type') == pred and v == obj]
+            Graph.remove_edges_from(edges_to_remove)
+
+            edges_to_remove = [(u, v) for u, v, attr in DiGraph.edges(data=True)
+            if attr.get('type') == pred and v == obj]
+            DiGraph.remove_edges_from(edges_to_remove)
+
+        elif 'license' in pred :
+            edges_to_remove = [(u, v) for u, v, attr in Graph.edges(data=True)
+            if attr.get('license') == pred and v == obj]
+            Graph.remove_edges_from(edges_to_remove)
+
+            edges_to_remove = [(u, v) for u, v, attr in DiGraph.edges(data=True)
+            if attr.get('license') == pred and v == obj]
+            DiGraph.remove_edges_from(edges_to_remove)
+
+        elif '"' in obj:
+            edges_to_remove = [(u, v) for u, v, attr in Graph.edges(data=True)
+            if attr.get('license') == pred and v == obj]
+            Graph.remove_edges_from(edges_to_remove)
+
+            edges_to_remove = [(u, v) for u, v, attr in DiGraph.edges(data=True)
+            if attr.get('license') == pred and v == obj]
+            DiGraph.remove_edges_from(edges_to_remove)
+
+        else :
+
+            Graph.add_edge(str(subj),str(obj),label=str(pred))
+            DiGraph.add_edge(str(subj),str(obj),label=str(pred))
+
+            #my_subj.append(subj)
+            #my_pred=[]
+            #my_obj=[]
+
+            print('subj',subj)
+            print('pred',pred)
+            print('object',obj)
+
     print(f'Knowledge Graph : {file_name}')
     print('Number of Nodes :',DiGraph.number_of_nodes())
     print('Number of edges :',DiGraph.number_of_edges())
     print('Is the graph is directed ? :',DiGraph.is_directed())
 
-    if DiGraph.is_directed() is False : #is_connected and components not implemented for directed Graph
-        print('Is the graph is connected ? :',nx.is_connected(DiGraph))
-        print('How many component ? :',nx.number_connected_components(DiGraph))
+    #if DiGraph.is_directed() is False : #is_connected and components not implemented for DiGraph
+        #print('Is the graph is connected ? :',nx.is_connected(DiGraph))
+        #print('How many component ? :',nx.number_connected_components(DiGraph))
 
     print('Is the graph is multigraph ? :',DiGraph.is_multigraph())
     #print('Is the graph is strongly connected ? :',nx.is_strongly_connected(G))
@@ -74,7 +124,7 @@ for file in all_files :
     print('Node connectivity approx',approximation.node_connectivity(DiGraph))
     #print('components',approximation.k_components(G))
     #print('maximum independent set',nx.approximation.maximum_indep
-   
+
     #print("degree",G.degree())
     #print("in degree",G.in_degree())
     #print("out degree",G.out_degree())
@@ -82,7 +132,11 @@ for file in all_files :
     #print("average node connectivity", average_node_connectivity(Graph))
     print("degree_histogram", degree_histogram(DiGraph))
 
-    #print(list(DiGraph.nodes()))
+    print(list(DiGraph.nodes()))
+    print("number of nodes : ", len(DiGraph.nodes()))
+    print(list(DiGraph.edges()))
+    print("number of edges : ", len(DiGraph.edges()))
+    #print(my_subj)
 
     #for node in DiGraph.nodes() :
     #    print(node)
@@ -103,4 +157,4 @@ for file in all_files :
     full_path=os.path.abspath(OUTPUTFILE)
     #print('full path',full_path)
 
-    #webbrowser.open(f'file://///wsl.localhost/Ubuntu-24.04{full_path}',autoraise=True)
+    webbrowser.open(f'file://///wsl.localhost/Ubuntu-24.04{full_path}',autoraise=True)
