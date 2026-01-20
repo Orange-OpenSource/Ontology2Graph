1fa722589848b9edfcc740ca3087621ddb143f3c
Philippe Dooze <philippe.dooze@orange.com>
2026-01-20 17:59:57 +0100

    modify function for owl-cli

--- utils_common/validate_ttl.py
+++ utils_common/validate_ttl.py
@@ -15,6 +15,7 @@ import subprocess
 # set argument parser #
 parser = argparse.ArgumentParser()
 parser.add_argument("PATH", help="Path where the files to validate are stored")
+parser.add_argument("mode", help="Turtle validation or formatting")
 args = parser.parse_args()
 
 all_files = [f.name for f in Path(args.PATH).iterdir() if f.is_file()]
@@ -22,10 +23,25 @@ all_files = [f.name for f in Path(args.PATH).iterdir() if f.is_file()]
 for i, file in enumerate(all_files):
     all_files[i]= args.PATH + file
 
-# check each ttl file with ttl validator #
-for file in all_files :
-    command=["ttl",file]
-    ttlvalidator=subprocess.Popen(command, stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
-    stdout, stderr = ttlvalidator.communicate()
-    print(file)
-    print(f'Turtle validator Result: {ttlvalidator.communicate()} \n')
+# format each ttl file with owl-cli #
+if args.mode=="format":
+    for file_to_validate in all_files :
+        command_format_ttl=["owl write --endOfLine lf --encoding utf_8 --indent space\
+            --input TURTLE --output TURTLE", file_to_validate, file_to_validate]
+        ttlvalidator=subprocess.Popen(command_format_ttl, stdout=subprocess.PIPE,\
+            stderr=subprocess.PIPE,text=True)
+        stdout, stderr = ttlvalidator.communicate()
+        #print(file)
+        #print(f'Turtle validator Result: {ttlvalidator.communicate()} \n')
+
+        # check each ttl file with ttl validator #
+elif args.mode=="validate":
+    for file in all_files :
+        command_validate_ttl=["ttl",file]
+        ttlvalidator=subprocess.Popen(command_validate_ttl, stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+        stdout, stderr = ttlvalidator.communicate()
+        print(file)
+        print(f'Turtle validator Result: {ttlvalidator.communicate()} \n')
+
+elif args.mode!="validate" and args.mode!="format":
+    print("Please use a correct mode argument value : validate or format")
